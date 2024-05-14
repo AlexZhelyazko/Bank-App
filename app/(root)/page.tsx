3b29baaeecba7b0d8811f5ab1ae61a3cb53d0978
Alex <aleexlemien@gmail.com>
2024-05-14 13:32:25 +0400

    add: home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
-const Page = () => {
-  return <div>Page</div>;
+const Home = () => {
+  const loggedIn = { firstName: "Alex" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Acess and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
-export default Page;
+export default Home;
